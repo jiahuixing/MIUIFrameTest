4d7b07bd4b30fb505b11e5e03e559b90ef239dde
jiahuixing <jiahuixing@gmail.com>
2015-07-05 16:03:14 +0800

    add test cases

--- app/src/androidTest/java/com/android/calculator/TestCalculator.java
+++ app/src/androidTest/java/com/android/calculator/TestCalculator.java
@@ -7,11 +7,15 @@ package com.android.calculator;
  * Created at 2015--07-05 15:54
  */
 
+import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
 import android.test.InstrumentationTestCase;
 
 import com.miui.marmot.lib.Checker;
 import com.miui.marmot.lib.Marmot;
+
+import java.util.Random;
 
 public class TestCalculator extends InstrumentationTestCase {
 
@@ -36,6 +40,17 @@ public class TestCalculator extends InstrumentationTestCase {
         marmot.launchActivity(ACTIVITY_NAME_CALCULATOR);
         marmot.waitFor(2);
         checker.assertTrue("calculator not launched", marmot.getCurrentPackageName().equals(PACKAGE_NAME_CALCULATOR));
+        UiObject2 buttons, button;
+        buttons = marmot.getUiObject(By.clazz("").res(""));
+        Random random = new Random();
+        int buttonCount, rnd;
+        buttonCount = buttons.getChildCount();
+        for (int i = 0; i < buttonCount; i++) {
+            rnd = random.nextInt(buttonCount);
+            button = buttons.getChildren().get(rnd);
+            button.click();
+            marmot.waitFor(0.5);
+        }
     }
 
     @Override
